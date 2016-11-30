b422a1eef70ca0224b9eea45d5acc42cb9f72c45
appERP <pelong82@gmail.com>
2016-11-30 08:41:34 -0800

    FIXED SQLITE

--- truckpro/settings/production.py
+++ truckpro/settings/production.py
@@ -6,12 +6,8 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'truckpro',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
